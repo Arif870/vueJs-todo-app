92bd80175c067bf6e83bb7226765e2f5196efb6c
Arif870 <arifuzzaman959@gmail.com>
2021-12-11 20:35:09 +0600

    click button added

--- vue.js
+++ vue.js
@@ -1,9 +1,25 @@
 let app = new Vue({
   el: "#app",
   data: {
-    form: null,
-    align: null,
-    fsize: null,
-    color: null,
+    foods: ["alo", "potol", "lao", "komra"],
+    alert: {
+      success: false,
+      warning: false,
+      danger: false,
+      msg: null,
+    },
+    newfood: null,
+  },
+  methods: {
+    addfood: function () {
+      if (this.newfood === null) {
+        this.alert.danger = true;
+        this.alert.msg = "field must not be empty !";
+      } else if (this.newfood != null) {
+        this.alert.danger = false;
+        this.alert.success = true;
+        this.alert.msg = this.newfood + " added successfully";
+      }
+    },
   },
 });
